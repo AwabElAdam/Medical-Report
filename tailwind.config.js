78fd5ba72c871b283ccff0c60f0d1b4c9285f04c
AwabElAdam <a.awab@saphiredent.com>
2024-03-11 14:41:21 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["**/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'elmessiri':["El Messiri"]
+      },
+      colors:{
+        'SaphireBlue':"#2A3C47",
+        'SaphireOrange':"#E18134",
+      }
+    },
+  },
+  plugins: [],
+}
+
